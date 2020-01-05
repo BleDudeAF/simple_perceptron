9594b6060e0c078c93c4156c33589a899b1f8b80
BleDudeAF <philippschoch99@gmail.com>
2020-01-05 15:06:18 +0100

    adding py

---
+++ perceptron.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+training_inputs = np.array([[0,0,1], [1,1,1], [1,0,1],[0,1,1]])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+print(training_inputs)
+print(training_outputs)
